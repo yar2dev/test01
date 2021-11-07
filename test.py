dc8c43bb548114e5f09f4ed814b8f71c1a3f6993
Yaroslav Ermilov <yarkiy.dev@gmail.com>
2021-11-07 16:53:57 +0300

    test

---
+++ test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+pd.options.mode.chained_assignment = None
+import numpy as np
+import datetime
+
+from pycbrf.toolbox import ExchangeRates
+import yfinance as yf
+import requests
+import time
+import urllib.request
+import io
+import smtplib
+import sys
+
+text_set1 = set ()
+text_set2 = set ()
+
