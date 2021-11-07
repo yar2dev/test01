ac3b5312eb29fe94a0b51389b70787e7184f75bd
Yaroslav Ermilov <yarkiy.dev@gmail.com>
2021-11-07 17:50:21 +0300

    test

--- test.py
+++ test.py
@@ -1,16 +1,6 @@
-import pandas as pd
-import numpy as np
 import datetime
 
-from pycbrf.toolbox import ExchangeRates
-import yfinance as yf
-import requests
-import time
-import urllib.request
-import io
-import smtplib
-import sys
-pd.options.mode.chained_assignment = None
 
 text_set1 = set()
 text_set2 = set()
+print("Checking...", datetime.datetime.now().time(), "\n")
